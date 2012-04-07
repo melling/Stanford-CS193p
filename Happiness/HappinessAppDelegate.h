4d494a2aba41d6f3339c9c1c0c2c3c2ff75419a1
Michael Mellinger <mmellinger66@gmail.com>
2012-04-07 18:35:17 -0400

    Initial Commit

---
+++ Happiness/HappinessAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HappinessAppDelegate.h
+//  Happiness
+//
+//  Created by Michael Mellinger on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Happiness/HappinessViewController.m
@@ -0,0 +1,34 @@
+//
+//  HappinessViewController.m
+//  Happiness
+//
+//  Created by Michael Mellinger on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HappinessViewController.h"
+
+@interface HappinessViewController ()
+
+@end
+
+@implementation HappinessViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
