bcaccca1bbbcb469be26a64dd2e21edd1af61ab9
Michael Mellinger <mmellinger66@gmail.com>
2012-04-08 00:27:12 -0400

    In next lesson we subclass UIViewController simply to make view rotatable

---
+++ Psychologist/Psychologist/RotableViewController.m
@@ -0,0 +1,23 @@
+//
+//  RotableViewController.m
+//  Psychologist
+//
+//  Created by Michael Mellinger on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RotableViewController.h"
+
+@interface RotableViewController ()
+
+@end
+
+@implementation RotableViewController
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
