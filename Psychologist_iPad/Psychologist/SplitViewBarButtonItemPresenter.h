e0ffe60fe03e28166034b4c3de891a88cc56fa1f
Michael Mellinger <mmellinger66@gmail.com>
2012-04-08 19:13:06 -0400

    Almost done with Lesson 7

---
+++ Psychologist_iPad/Psychologist/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,14 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Psychologist
+//
+//  Created by Michael Mellinger on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+@end
